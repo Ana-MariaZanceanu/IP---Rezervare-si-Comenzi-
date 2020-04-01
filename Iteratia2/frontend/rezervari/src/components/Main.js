22139c5b52a71cb80cc2c9fd0e2bbded8ad1b059
Raluca Filimon <filimon.raluca@gmail.com>
2020-04-01 16:12:45 +0300

    Clean

---
+++ Iteratia2/frontend/rezervari/src/components/Main.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+export class MainComp extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Main</h1>
+      </div>
+    );
+  }
+}
+
+export default MainComp;
