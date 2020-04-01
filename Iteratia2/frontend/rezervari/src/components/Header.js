abc5d1b27b0ddad917dcc377c38d82463be06a4d
Stan Laura-Ioana <laurastan1999@gmail.com>
2020-04-01 20:30:03 +0300

    update

--- Iteratia2/frontend/rezervari/src/components/Header.js
+++ Iteratia2/frontend/rezervari/src/components/Header.js
@@ -1,26 +1,35 @@
 import React, { Component } from 'react';
-
+import logo from './cont.png';
 export class HeaderComp extends Component {
   render() {
     return (
-      <header>
-        <ul>
-          <li>
-            <a className="active" href="#home">
-              Home
-            </a>
-          </li>
-          <li>
-            <a href="#news">News</a>
-          </li>
-          <li>
-            <a href="#contact">Contact</a>
-          </li>
-          <li>
-            <a href="#about">About</a>
-          </li>
-        </ul>
-      </header>
+      <div className="header">
+        <div className="header-left">
+          <ul className="header-ul">
+            <li className="header-li">
+              <a className="active-home" href="#home">
+                Home
+              </a>
+            </li>
+            <li className="header-li">
+              <a href="#news">News</a>
+            </li>
+            <li className="header-li">
+              <a href="#contact">Contact</a>
+            </li>
+            <li className="header-li">
+              <a href="#about">About</a>
+            </li>
+          </ul>
+        </div>
+        <div className="header-center"></div>
+        <div className="header-right">
+          <div class="cont-logo">
+            <img src={logo} alt="logo" />
+            <a>Hello,Visitor!</a>
+          </div>
+        </div>
+      </div>
     );
   }
 }
