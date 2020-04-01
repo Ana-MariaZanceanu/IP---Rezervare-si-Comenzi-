34760d6c4dbb0e05d3338bcec028de4a955dddf8
Ana-MariaZanceanu <ana.zanceanu@yahoo.com>
2020-04-01 17:53:05 +0300

    main

--- Iteratia2/frontend/rezervari/src/components/Main.js
+++ Iteratia2/frontend/rezervari/src/components/Main.js
@@ -3,8 +3,45 @@ import React, { Component } from "react";
 export class MainComp extends Component {
   render() {
     return (
-      <div>
-        <h1>Main</h1>
+      <div className="main">
+        <div className="restaurant_div">
+            <div className="restaurant_info">
+                <h2>Restaurant 1</h2>
+            </div>
+            <div className="restaurant_buttons">
+                <button className="button" id="rezerva_button">Rezervă</button>
+                <button className="button" id="comanda_button">Comandă</button>
+            </div>
+
+        </div>
+        <div className="restaurant_div">
+            <div className="restaurant_info">
+                <h2>Restaurant 2</h2>
+            </div>
+            <div className="restaurant_buttons">
+                <button className="button" id="rezerva_button">Rezervă</button>
+                <button className="button" id="comanda_button">Comandă</button>
+            </div>
+        </div>
+        <div className="restaurant_div">
+            <div className="restaurant_info">
+                <h2>Restaurant 3</h2>
+            </div>
+            <div className="restaurant_buttons">
+                <button className="button" id="rezerva_button">Rezervă</button>
+                <button className="button" id="comanda_button">Comandă</button>
+            </div>
+
+        </div>
+        <div className="restaurant_div">
+            <div className="restaurant_info">
+                <h2>Restaurant 4</h2>
+            </div>
+            <div className="restaurant_buttons">
+                <button className="button" id="rezerva_button">Rezervă</button>
+                <button className="button" id="comanda_button">Comandă</button>
+            </div>
+        </div>
       </div>
     );
   }
