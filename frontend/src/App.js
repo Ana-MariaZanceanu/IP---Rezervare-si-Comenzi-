4a8acd98aae7895f66462d672d38cd79452bb6cb
Stan Laura-Ioana <laurastan1999@gmail.com>
2020-04-15 20:39:03 +0300

    update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import ModalForm from './components/ModalForm';
+import PersonalData from './components/personalData/PersonalData';
 
 function App() {
   return (
     <div>
       <ModalForm name="Book a table" />
+      <PersonalData />
     </div>
   );
 }
