7883a6285bc83f2419b0d75b827e830f47563e20
Ana-MariaZanceanu <ana.zanceanu@yahoo.com>
2020-05-01 12:54:59 +0300

    comm

---
+++ frontend/src/components/productModal/ModalProduct.js
@@ -0,0 +1,15 @@
+import React, { useState }, {Component} from 'react';
+import Modal from 'react-bootstrap/Modal';
+
+class ModalProduct extends Component {
+    render() {
+        return (
+            <div>
+
+            </div>
+        );
+    }
+}
+
+
+export default ModalProduct;
