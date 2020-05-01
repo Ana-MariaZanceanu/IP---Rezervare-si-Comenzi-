153a039b75fb166bac81888e12df4d298ac09ff1
Stan Laura-Ioana <laurastan1999@gmail.com>
2020-05-01 17:51:10 +0300

    update

--- frontend/src/components/productModal/ModalProduct.js
+++ frontend/src/components/productModal/ModalProduct.js
@@ -1,23 +1,36 @@
-import React, { useState } from "react";
-import Modal from "react-bootstrap/Modal";
-import Container from "react-bootstrap/Container";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
-import Card from "react-bootstrap/Card";
-import Image from "react-bootstrap/Image";
-import "../../App.css";
-
-
+import React, { useState } from 'react';
+import Modal from 'react-bootstrap/Modal';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Card from 'react-bootstrap/Card';
+import Image from 'react-bootstrap/Image';
+import '../../App.css';
+import { MDBIcon, MDBBtn } from 'mdbreact';
+import Button from 'react-bootstrap/Button';
+import { FaCartPlus,FaHeart } from 'react-icons/fa';
 export default function ModalProduct(props) {
-  const {show,onHide,img,productName,productPrice,productAvailability} = props;
+  const {
+    show,
+    onHide,
+    img,
+    productName,
+    productPrice,
+    productAvailability,
+  } = props;
   return (
-    <Modal show={show} onHide={onHide} dialogClassName="modalSizes" aria-labelledby="example-custom-modal-styling-title">
+    <Modal
+      show={show}
+      onHide={onHide}
+      dialogClassName="modalSizes"
+      aria-labelledby="example-custom-modal-styling-title"
+    >
       <Modal.Header closeButton style={styles.modalHeader} />
       <Modal.Body>
         <Container>
           <Row className="show-grid">
             <Col xs={12} md={5}>
-                  <Image src={img} rounded style={styles.imageStyle} />
+              <Image src={img} rounded style={styles.imageStyle} />
             </Col>
             <Col xs={12} md={6}>
               <Row className="show-grid">
@@ -34,11 +47,15 @@ export default function ModalProduct(props) {
                 </Col>
               </Row>
               <Row className="show-grid">
-                <Col xs={5} md={6}>
-                  <Card.Text>add to cart</Card.Text>
+                <Col xs={5} md={5}>
+                  <Button variant="outline-success">
+                    <FaCartPlus /> Add to cart
+                  </Button>
                 </Col>
-                <Col xs={5} md={6}>
-                  <Card.Text>add to wishlist</Card.Text>
+                <Col xs={5} md={5}>
+                  <Button variant="outline-danger">
+                    <FaHeart/> Add to wishlist
+                  </Button>
                 </Col>
               </Row>
             </Col>
@@ -57,14 +74,14 @@ export default function ModalProduct(props) {
 
 const styles = {
   modalHeader: {
-    border: "none",
+    border: 'none',
   },
   nameStyle: {
-    fontFamily: "Pacifico",
-    fontSize: "2rem",
+    fontFamily: 'Pacifico',
+    fontSize: '2rem',
   },
   imageStyle: {
-    width: "100%",
-    height: "100%"
+    width: '100%',
+    height: '100%',
   },
 };
