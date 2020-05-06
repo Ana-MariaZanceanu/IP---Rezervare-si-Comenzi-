84a596d4e15df0edead4b224267fb89922fa60ec
Stan Laura-Ioana <laurastan1999@gmail.com>
2020-05-06 18:59:43 +0300

    update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,14 +7,16 @@ import Container from 'react-bootstrap/Container';
 import ShowModalProduct from './components/productModal/ShowModalProduct';
 import MainPage from './components/mainPage/MainPage';
 import pizzaImage from './components/productModal/images/pizza.png';
+import ShoppingCart from './components/shoppingCart/ShoppingCart';
 function App() {
   return (
     <div>
-      {/*<ModalForm name="Book a table" />*/}
+      {/*<ModalForm name="Book a table" />
 
       <ShowModalProduct img={pizzaImage} productName={"Pizza"} productPrice={"12$"} productAvailability={"in stock"}/>
         <ShowModalProduct img={pizzaImage} productName={"Pizza"} productPrice={"12$"} productAvailability={"sold out"}/>
-
+*/}
+      <ShoppingCart />
       {/*<Container>
         <Order />
       </Container>*/}
