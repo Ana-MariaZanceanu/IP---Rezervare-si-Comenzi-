7bd94e5e7c4cb20df784ce3da460c4017785e9cc
Raluca Filimon <filimon.raluca@gmail.com>
2020-05-07 15:35:31 +0300

    added async

--- frontend/src/components/mainPage/ChooseUsRestaurants.js
+++ frontend/src/components/mainPage/ChooseUsRestaurants.js
@@ -15,39 +15,37 @@ export class ChooseUsRestaurants extends Component {
     };
   }
 
-  componentDidMount() {
-    fetch("http://localhost:4000/api/providers")
+  async componentDidMount() {
+    await fetch("http://localhost:4000/api/providers")
       .then((res) => res.json())
-      .then(
-        (result) => {
-          this.setState({
-            isLoaded: true,
-            items: result.data.providers,
-          });
-          const restaurantsCopy = [];
-          for (var i = 0; i < 3; i++) {
-            const name = this.state.items[i % 2].name;
-            const img = this.state.items[i % 2].details.images[0];
-            console.log(img);
-            const desc = this.state.items[i % 2].details.description.substring(
-              1,
-              150
-            );
-            restaurantsCopy.push(
-              <Col md>
-                <RestaurantCard title={name} img={img} desc={desc} />
-              </Col>
-            );
-          }
-          this.setState({ restaurants: restaurantsCopy });
-        },
-        (error) => {
-          this.setState({
-            isLoaded: true,
-            error,
-          });
+      .then((result) => {
+        this.setState({
+          isLoaded: true,
+          items: result.data.providers,
+        });
+        const restaurantsCopy = [];
+        for (var i = 0; i < 3; i++) {
+          const name = this.state.items[i % 2].name;
+          const img = this.state.items[i % 2].details.images[0];
+          console.log(img);
+          const desc = this.state.items[i % 2].details.description.substring(
+            1,
+            150
+          );
+          restaurantsCopy.push(
+            <Col md>
+              <RestaurantCard title={name} img={img} desc={desc} />
+            </Col>
+          );
         }
-      );
+        this.setState({ restaurants: restaurantsCopy });
+      })
+      .catch((error) => {
+        this.setState({
+          isLoaded: true,
+          error,
+        });
+      });
   }
   render() {
     return (
