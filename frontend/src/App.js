8bf28bc929ef9323d2cbe3dd3d0d83237045b89e
Ana-MariaZanceanu <ana.zanceanu@yahoo.com>
2020-05-08 11:08:50 +0300

    comm

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,22 +7,21 @@ import Container from 'react-bootstrap/Container';
 import ShowModalProduct from './components/productModal/ShowModalProduct';
 import MainPage from './components/mainPage/MainPage';
 import pizzaImage from './components/productModal/images/pizza.png';
-import ShoppingCart from './components/shoppingCart/ShoppingCart';
-function App() {
-  return (
-    <div>
-      {/*<ModalForm name="Book a table" />
 
-      <ShowModalProduct img={pizzaImage} productName={"Pizza"} productPrice={"12$"} productAvailability={"in stock"}/>
-        <ShowModalProduct img={pizzaImage} productName={"Pizza"} productPrice={"12$"} productAvailability={"sold out"}/>
-*/}
-      <ShoppingCart />
-      {/*<Container>
+// import ShoppingCart from './components/shoppingCart/ShoppingCart';
+function App() {
+    return (
+        <div>
+            {/*<ModalForm name="Book a table" />*/}
+
+            <ShowModalProduct id={"5eb17a5c6f436666294bc421"} productAvailability={"in stock"}/>
+            {/*<ShoppingCart />*/}
+            {/*<Container>
         <Order />
       </Container>*/}
-      {/*<MainPage />*/}
-    </div>
-  );
+            {/*<MainPage />*/}
+        </div>
+    );
 }
 
 export default App;
--- frontend/src/components/productModal/ModalProduct.js
+++ frontend/src/components/productModal/ModalProduct.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, {Component} from 'react';
 import Modal from 'react-bootstrap/Modal';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
@@ -6,70 +6,93 @@ import Col from 'react-bootstrap/Col';
 import Card from 'react-bootstrap/Card';
 import Image from 'react-bootstrap/Image';
 import Button from 'react-bootstrap/Button';
-import { FaCartPlus,FaHeart, FaCheck } from 'react-icons/fa';
+import {FaCartPlus, FaHeart, FaCheck} from 'react-icons/fa';
 import ListGroup from "react-bootstrap/ListGroup";
 import "./style.css";
+import axios from "axios";
 
-export default function ModalProduct(props) {
-  const {show,onHide,img,productName,productPrice,productAvailability} = props;
-  let availabilityStyle;
-  if(productAvailability === "in stock"){
-    availabilityStyle = "inStock";
-  }else{
-    availabilityStyle = "soldOut";
-  }
-  return (
-    <Modal show={show} onHide={onHide} dialogClassName="modalSizes" aria-labelledby="example-custom-modal-styling-title">
-      <Modal.Header closeButton className={"modalHeader"} />
-      <Modal.Body>
-        <Container>
-          <Row className="show-grid">
-            <Col xs={12} md={5}>
-                  <Image src={img} rounded className={"imageStyle"} />
-            </Col>
-            <Col xs={12} md={7}>
-              <Row className="show-grid">
-                <Col xs={12} md={12}>
-                  <h2 className={"nameStyle"}>{productName}</h2>
-                </Col>
-              </Row>
-              <Row className="show-grid">
-                <Col xs={5} md={12}>
-                  <Card.Text className={"priceStyle"}>{productPrice}</Card.Text>
-                </Col>
-                <Col xs={5} md={12}>
-                  <Card.Text className={availabilityStyle}>{productAvailability}</Card.Text>
-                </Col>
-              </Row>
-              <Row className="show-grid buttons_cart">
-                <Col xs={5} md={5}>
-                  <Button variant="outline-success">
-                    <FaCartPlus /> Add to cart
-                  </Button>
-                </Col>
-                <Col xs={6} md={6}>
-                  <Button variant="outline-danger">
-                    <FaHeart/> Add to wishlist
-                  </Button>
-                </Col>
-              </Row>
-            </Col>
-          </Row>
+class ModalProduct extends Component{
 
-          <Row className="show-grid">
-            <Col xs={12} md={12}>
-              <Card.Text className={"descriptionTitleStyle"}>Product Ingredients</Card.Text>
-              <ListGroup className={"descriptionTextStyle"}>
-                <ListGroup.Item><FaCheck/>Ingredient 1</ListGroup.Item>
-                <ListGroup.Item><FaCheck/>Ingredient 2</ListGroup.Item>
-                <ListGroup.Item><FaCheck/>Ingredient 3</ListGroup.Item>
-                <ListGroup.Item><FaCheck/>Ingredient 4</ListGroup.Item>
-                <ListGroup.Item><FaCheck/>Ingredient 5</ListGroup.Item>
-              </ListGroup>
-            </Col>
-          </Row>
-        </Container>
-      </Modal.Body>
-    </Modal>
-  );
+    constructor(props) {
+        super(props);
+        let message = "";
+    }
+
+    postProductToCart = async (idProduct) => {
+        await axios({
+            method: "get",
+            url: "http://localhost:3000/api/v1/cart/add-product/" + idProduct
+        }).then((response) => {
+            console.log(response)
+        }).catch((error) => {
+            console.log(error)
+        });
+    }
+
+    render(){
+        const {show,onHide,productAvailability} = this.props;
+        const {product} = this.props;
+        let availabilityStyle;
+        if (productAvailability === "in stock") {
+            availabilityStyle = "inStock";
+        } else {
+            availabilityStyle = "soldOut";
+        }
+        if(Object.keys(product).length === 0){
+            return <div/>
+        }
+        return (
+            <Modal show={show} onHide={onHide} dialogClassName="modalSizes"
+                   aria-labelledby="example-custom-modal-styling-title">
+                <Modal.Header closeButton className={"modalHeader"}/>
+                <Modal.Body>
+                    <Container>
+                        <Row className="show-grid">
+                            <Col xs={12} md={5}>
+                                <Image src={product.image} rounded className={"imageStyle"} />
+                            </Col>
+                            <Col xs={12} md={7}>
+                                <Row className="show-grid">
+                                    <Col xs={12} md={12}>
+                                        <h2 className={"nameStyle"}>{product.name}</h2>
+                                    </Col>
+                                </Row>
+                                <Row className="show-grid">
+                                    <Col xs={5} md={12}>
+                                        <Card.Text className={"priceStyle"}>{product.price}$</Card.Text>
+                                    </Col>
+                                    <Col xs={5} md={12}>
+                                        <Card.Text className={availabilityStyle}>{productAvailability}</Card.Text>
+                                    </Col>
+                                </Row>
+                                <Row className="show-grid buttons_cart">
+                                    <Col xs={5} md={5}>
+                                        <Button variant="outline-success" onClick={() => {this.postProductToCart(product._id)}}>
+                                            <FaCartPlus/> Add to cart
+                                        </Button>
+                                    </Col>
+                                    <Col xs={6} md={6}>
+                                        <Button variant="outline-danger">
+                                            <FaHeart/> Add to wishlist
+                                        </Button>
+                                    </Col>
+                                </Row>
+                            </Col>
+                        </Row>
+
+                        <Row className="show-grid">
+                            <Col xs={12} md={12}>
+                                <Card.Text className={"descriptionTitleStyle"}>Product Ingredients</Card.Text>
+                                <ListGroup className={"descriptionTextStyle"}>
+                                    {product.ingredients.map(ingredient => <ListGroup.Item><FaCheck/>{ingredient}</ListGroup.Item>)}
+                                </ListGroup>
+                            </Col>
+                        </Row>
+                    </Container>
+                </Modal.Body>
+            </Modal>
+        );
+    }
 }
+
+export default ModalProduct;
